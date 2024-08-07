ea3d46d26ad99228f9378cf132b1013d2d4f904a
AqibNiazi <aqibjaved5201@gmail.com>
2024-08-07 22:40:27 +0500

    project homepaged changed to tackle api response

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,35 +1,18 @@
 import React, { useState } from "react";
-import { FileUploader, Form } from "../FormControls";
-import { Button } from "../UI";
-import { clientBaseURL, clientEndPoints } from "../../config";
+import { FileUploader, Form } from "@components/FormControls";
+import { Button } from "@components/UI";
+import { clientBaseURL, clientEndPoints } from "@/config";
 import toast from "react-hot-toast";
-import SummaryForm from "../SummaryForm";
-import QuestionsForm from "../QuestionsForm";
-import EntityExtractionForm from "../EntityExtrationForm";
-import { useQueryContext } from "../../store/QueryContext";
-import useTypingEffect from "../../hook";
+import useTypingEffect from "@hook"; // Adjust the import path as necessary
 
 const Home = () => {
   const [document, setDocument] = useState(null);
   const [loading, setLoading] = useState(false);
   const [successMessage, setSuccessMessage] = useState("");
-  const [currentResponseType, setCurrentResponseType] = useState(null); // Manage the current response type
-  const { queryResponses } = useQueryContext();
+  const [response, setResponse] = useState(null);
+  const [currentResponseType, setCurrentResponseType] = useState(null);
 
-  const displayedQueryResponse = useTypingEffect(
-    JSON.stringify(queryResponses.question_answer, null, 2),
-    10
-  );
-
-  const displayedEntityResponse = useTypingEffect(
-    JSON.stringify(queryResponses.entity_extraction, null, 2),
-    10
-  );
-
-  const displayedSummaryResponse = useTypingEffect(
-    JSON.stringify(queryResponses.summary, null, 2),
-    10
-  );
+  const typedSummary = useTypingEffect(response?.summary, 20);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -41,6 +24,8 @@ const Home = () => {
     }
 
     try {
+      setResponse(null);
+      setCurrentResponseType(null);
       const response = await clientBaseURL.post(
         `${clientEndPoints?.uploadDoc}`,
         formData,
@@ -50,11 +35,12 @@ const Home = () => {
           },
         }
       );
-
+      console.log("Response in home :", response);
       if (response.status >= 200 && response.status < 300) {
         toast.success("File Uploaded Successfully");
         setSuccessMessage("Your document has been uploaded successfully.");
         setDocument(null);
+        setResponse(response.data); // Store the response data
       }
     } catch (error) {
       if (error.response) {
@@ -65,6 +51,38 @@ const Home = () => {
     } finally {
       setLoading(false);
     }
+  };
+
+  const renderResponse = () => {
+    if (!response) return null;
+
+    if (currentResponseType === "summary") {
+      return (
+        <div className="bg-white rounded-md border-2 border-dashed border-blue-600 shadow-md p-4 mt-6 w-full">
+          <p className="text-center text-xl font-bold mb-2 text-blue-600">
+            Summary of Document
+          </p>
+          <pre className="overflow-auto whitespace-pre-wrap text-lg text-gray-800 leading-6">
+            {typedSummary}
+          </pre>
+        </div>
+      );
+    }
+
+    if (currentResponseType === "entity_extraction") {
+      return (
+        <div className="bg-white rounded-md border-2 border-dashed border-blue-600 shadow-md p-4 mt-6 w-full">
+          <p className="text-center text-xl font-bold mb-2 text-blue-600">
+            Entities Extracted from Document.
+          </p>
+          <pre className="overflow-auto whitespace-pre-wrap text-lg text-gray-800 leading-6">
+            {response?.entities.join("\n")}
+          </pre>
+        </div>
+      );
+    }
+
+    return null;
   };
 
   return (
@@ -105,67 +123,29 @@ const Home = () => {
           </p>
           <p className="text-gray-800 font-medium text-lg">Now you can</p>
           <ul className="text-center mb-4 text-gray-800 text-base font-semibold">
-            <li>Analyze the document for key information.</li>
-            <li>Extract entities from the document.</li>
-            <li>Generate summaries and insights.</li>
-            <li>Ask questions about the document.</li>
+            <li>Generate Summery of document.</li>
+            <li>Extract Entities of the document.</li>
           </ul>
+          <div className="flex justify-center space-x-4">
+            <Button
+              onClick={() => {
+                setCurrentResponseType("summary");
+                setResponse(response); // Ensure response state is passed to useTypingEffect
+              }}
+              className="text-white px-4 py-1"
+            >
+              Generate Summary
+            </Button>
+            <Button
+              onClick={() => setCurrentResponseType("entity_extraction")}
+              className="text-white px-4 py-1"
+            >
+              Extract Entities
+            </Button>
+          </div>
         </div>
       )}
-      <div className="flex flex-col sm:flex-row justify-between sm:mb-8">
-        <SummaryForm onSelect={() => setCurrentResponseType("summary")} />
-        <EntityExtractionForm
-          onSelect={() => setCurrentResponseType("entity_extraction")}
-        />
-        <QuestionsForm
-          onSelect={() => setCurrentResponseType("question_answer")}
-        />
-      </div>
-
-      {/* Display the response based on currentResponseType */}
-      {currentResponseType === "summary" && queryResponses.summary && (
-        <div
-          id="response_box"
-          className="bg-white rounded-md border-2 border-dashed border-blue-600 shadow-md p-4 mt-6 w-full"
-        >
-          <p className="text-center text-xl font-bold mb-2 text-blue-600">
-            AI Response for Summary
-          </p>
-          <pre className="overflow-auto whitespace-pre-wrap text-lg text-gray-800 leading-6">
-            {displayedSummaryResponse}
-          </pre>
-        </div>
-      )}
-
-      {currentResponseType === "entity_extraction" &&
-        queryResponses.entity_extraction && (
-          <div
-            id="response_box"
-            className="bg-white rounded-md border-2 border-dashed border-blue-600 shadow-md p-4 mt-6 w-full"
-          >
-            <p className="text-center text-xl font-bold mb-2 text-blue-600">
-              AI Response for Entity Extraction
-            </p>
-            <pre className="overflow-auto whitespace-pre-wrap text-lg text-gray-800 leading-6">
-              {displayedEntityResponse}
-            </pre>
-          </div>
-        )}
-
-      {currentResponseType === "question_answer" &&
-        queryResponses.question_answer && (
-          <div
-            id="response_box"
-            className="bg-white rounded-md border-2 border-dashed border-blue-600 shadow-md p-4 mt-6 w-full"
-          >
-            <p className="text-center text-xl font-bold mb-2 text-blue-600">
-              AI Response for Question
-            </p>
-            <pre className="overflow-auto whitespace-pre-wrap text-lg text-gray-800 leading-6">
-              {displayedQueryResponse}
-            </pre>
-          </div>
-        )}
+      {renderResponse()}
     </div>
   );
 };
